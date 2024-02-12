fa472cc9b10e48d68fbdf44f1d38e9b4290a2394
João Pedro de Melo França <joaopmfranca@gmail.com>
2024-02-12 20:09:41 -0300

    chore: page navegation

--- pages/about.js
+++ pages/about.js
@@ -1,8 +1,15 @@
+import Link from "next/link";
+
 export default function About() {
     return (
         <div>
             <h1>About</h1>
             <p>This is the about page</p>
+            <ul>
+                <li>
+                    <Link href="/">Voltar</Link>
+                </li>
+            </ul>
         </div>
     )
 }
--- pages/products/index.js
+++ pages/products/index.js
@@ -1,8 +1,13 @@
+import Link from "next/link";
+
 export default function Products() {
     return (
         <div>
             <h1>Products</h1>
             <p>This is the products page</p>
-        </div>
+            <ul>
+                <li><Link href="/">Voltar</Link></li>
+            </ul>
+        </div >
     )
 }
