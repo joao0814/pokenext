830a814bfae172a5a38879848536f0e3eba4ae44
João Pedro de Melo França <joaopmfranca@gmail.com>
2024-02-24 15:51:46 -0300

    chore: learning assets

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import styles from '../styles/HomePage.module.css'
 import Head from 'next/head'
 
@@ -11,6 +12,7 @@ export default function Home() {
       </Head>
       <div className={styles.home}>
         <h1>Hello World</h1>
+        <Image src="/images/city.jpg" width={400} height={500} alt='city in the night' />
       </div>
     </>
   )
